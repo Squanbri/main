b443b82492a378583f722998d3f0397dfe85b644
Squanbri <Squan-bri@mail.ru>
2020-11-09 03:31:49 +0300

    v3.0

---
+++ app/controllers/forms_controller.rb
@@ -0,0 +1,19 @@
+class FormsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    @form = Form.new(form_params)
+
+    if @form.save
+      render json: @form, status: :created
+      FormMailer.form_send(@form).deliver
+    else
+      render json: "Some error", status: :bad_request
+    end
+  end
+
+  private
+  def form_params
+    params.require(:form).permit(:fio, :phone, :company, :comment, :skills)
+  end
+end
